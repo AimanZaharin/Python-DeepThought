7e0c238f519463b81c442da84f3e76e88c571d65
Nor Aiman Zaharin bin Noor Azwan <92364588+AimanZaharin@users.noreply.github.com>
2022-08-07 16:24:24 +0800

    Add files via upload

---
+++ deep.py
@@ -0,0 +1,17 @@
+
+def main():
+    
+    answer = input("What is the Answer to the Great Question of Life, the Universe and Everything? ")
+
+    match answer:
+        case "42":
+            print("Yes...!")
+        case "forty-two":
+            print("Yes...!")
+        case "forty two":
+            print("Yes...!")
+        case _:
+            print("No")
+
+
+main()
